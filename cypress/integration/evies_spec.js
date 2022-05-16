f345c58148c7af2c396e81469ee105d0b71f0864
Evie Skinner <evie.skinner@madetech.com>
2022-05-16 11:27:00 +0100

    Added steps to navigate to Ocado, accept cookies, and log in. Logging in using env vars. Not working yet

--- cypress/integration/evies_spec.js
+++ cypress/integration/evies_spec.js
@@ -1,11 +1,58 @@
-describe('My First Test', () => {
-    it('Does not do much but I\'m just a legend HA!', () => {
-      expect(true).to.equal(true)
+  /// <reference types="Cypress" />
+
+  describe('Accept cookies from the Ocado homepage', () => {
+    it('Contains the content "Accept"', () => {
+      cy.visit('https://www.ocado.com')
+      cy.contains('Accept').click();
     })
   })
 
-  describe('My Failing Test', () => {
-    it('Ooh ahh I\'m a naughty failing test', () => {
-      expect(true).to.equal(false)
+  describe('Log in to Ocado', () => {
+    it('Contains the content "Log in"', () => {
+      cy.contains('Log in').click();
+    })
+  })
+
+  describe('Check we are now on the Login page', () => {
+    it('Url includes the string "login"', () => {
+      cy.url().should('include', '/login');
+    })
+  })
+
+  describe('Accept cookies from the Ocado login page', () => {
+    it('Contains the content "Accept"', () => {
+      cy.contains('Accept').click();
+    })
+  })
+
+  describe('Enter email', () => {
+    it('Can type into the input for "Email"', () => {
+        cy.get('.login__form')
+        .find('[type="email"]')
+        .type(Cypress.env("ciaras_email"))
+        .should('have.value', Cypress.env("ciaras_email"))
+    })
+  })
+
+  describe('Enter password', () => {
+    it('Can type into the input for "Password"', () => {
+        cy.get('.login__form')
+        .find('[type="password"]')
+        .type(Cypress.env("ciaras_password"))
+        .should('have.value', Cypress.env("ciaras_password"))
+    })
+  })
+
+  describe('Click Login button', () => {
+    it('Can submit the Login form', () => {
+        cy.get('.login__form')
+        .find('.login__submit')
+        .click();
+    })
+  })
+
+  describe('Re-accept cookies from the Ocado login page', () => {
+    it('Contains the content "Accept"', () => {
+      cy.contains('Accept').click();
     })
   })
