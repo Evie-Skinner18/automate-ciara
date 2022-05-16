d480782580b29ddc1dc0aefb9f21786aa88f9687
Evie Skinner <evie.skinner@madetech.com>
2022-05-16 10:08:33 +0100

    Initial commit

---
+++ cypress/integration/evies_spec.js
@@ -0,0 +1,11 @@
+describe('My First Test', () => {
+    it('Does not do much but I\'m just a legend HA!', () => {
+      expect(true).to.equal(true)
+    })
+  })
+
+  describe('My Failing Test', () => {
+    it('Ooh ahh I\'m a naughty failing test', () => {
+      expect(true).to.equal(false)
+    })
+  })
